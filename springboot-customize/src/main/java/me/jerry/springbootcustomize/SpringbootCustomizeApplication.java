687b276aa84547b5a10384c09214e49c9cc17a51
soojae <developersoojae@gmail.com>
2020-03-28 20:58:13 +0900

    [공부] 스프링부트 커스터마이즈

---
+++ springboot-customize/src/main/java/me/jerry/springbootcustomize/SpringbootCustomizeApplication.java
@@ -0,0 +1,39 @@
+package me.jerry.springbootcustomize;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
+
+@SpringBootApplication
+public class SpringbootCustomizeApplication {
+
+/**
+ * 기존 것을 그대로 사용하면 커스터마이즈 하기 힘들다.
+ */
+//  public static void main(String[] args) {
+//    SpringApplication.run(SpringbootCustomizeApplication.class, args);
+//  }
+
+  /**
+   * 이렇게 바꿔보자
+   */
+  public static void main(String[] args) {
+//    SpringApplication.run(SpringbootCustomizeApplication.class, args);
+
+//    SpringApplication app = new SpringApplication(SpringbootCustomizeApplication.class);
+//    app.run(args);
+
+    /**
+     * 3번째 방법
+     */
+    new SpringApplicationBuilder()
+            .sources(SpringbootCustomizeApplication.class)
+            .run(args);
+
+
+  }
+}
---
+++ springboot-customize/src/test/java/me/jerry/springbootcustomize/SpringbootCustomizeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jerry.springbootcustomize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCustomizeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
