dc84c98b86a1d9d246b89f78a109f3531dd49f6f
soojae <developersoojae@gmail.com>
2020-03-29 02:28:20 +0900

    [공부] 프로퍼티, 프로파일 설정

---
+++ outside-configuration/src/main/java/me/jerry/outsideconfiguration/config/BaseConfiguration.java
@@ -0,0 +1,14 @@
+package me.jerry.outsideconfiguration.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("prod")
+@Configuration
+public class BaseConfiguration {
+  @Bean
+  public String hello() {
+    return "hello prod";
+  }
+}
