fd74b232acd5104ce1c7669bd927847f71c669dc
soojae <developersoojae@gmail.com>
2020-03-29 21:37:31 +0900

    [공부] HttpMessageConverters

--- spring-mvc/src/main/java/me/jerry/springmvc/user/UserController.java
+++ spring-mvc/src/main/java/me/jerry/springmvc/user/UserController.java
@@ -1,7 +1,6 @@
 package me.jerry.springmvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -10,4 +9,15 @@ public class UserController {
   public String hello() {
     return "hello";
   }
+
+  @PostMapping("/users/create")
+  public User create(@RequestBody User user){
+    return user;
+  }
+
+//  @PostMapping("/user")
+//  public User create(@RequestBody User user){
+//    return null;
+//  }
+
 }
