b5b7aa81e53bf13ce8d5456bdd26fb72662b1389
soojae <developersoojae@gmail.com>
2020-03-31 08:20:35 +0900

    [공부] 스프링부트 공부

---
+++ jdbc/src/test/java/me/jerry/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jerry.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
