2e2f96ba179dbbdd0789eaf7d26e4a695ac5f89e
soojae <developersoojae@gmail.com>
2020-03-31 07:57:23 +0900

    [공부] 스프링부트 - CORS

---
+++ cors-client/src/test/java/me/jerry/corsclient/CorsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jerry.corsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
